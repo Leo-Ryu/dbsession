d6e05b2e91a7d404e92b08f8b2fa3233d4704ac8
imwiwiim90 <imwiwiim90@gmail.com>
2020-01-25 22:37:33 -0500

    wrapping example code in __main__ constraint

--- example/example.py
+++ example/example.py
@@ -2,9 +2,11 @@ from config import db_connection
 import A
 
 
-db_session = db_connection.get_session()
-a_1 = db_session.query(A.find_by_id, 1)
-print(a_1)
+if __name__ == "__main__":
+	
+	db_session = db_connection.get_session()
+	a_1 = db_session.query(A.find_by_id, 1)
+	print(a_1)
 
-all_a = db_session.query(A.get_all)
-print(all_a)
+	all_a = db_session.query(A.get_all)
+	print(all_a)
